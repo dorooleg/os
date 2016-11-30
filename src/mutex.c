34b559872600bd9b06ecd9175d56e3482e9417f6
Oleg Doronin <dorooleg@yandex.ru>
2016-11-30 20:59:44 +0300

    Mutex

--- src/mutex.c
+++ src/mutex.c
@@ -8,25 +8,26 @@ extern struct spinlock multithreading_lock;
 
 void mutex_lock(struct mutex_t * lock_)
 {
+    lock(&multithreading_lock);
     if (lock_->locked) {
-        lock(&multithreading_lock);
         struct thread_t * current_thread = thread_get_current();
         list_push_back(&lock_->locked_threads, current_thread);
         mutex_thread_yield();
     } else {
         lock_->locked = 1;
+        unlock(&multithreading_lock);
     }
 }
 
 void mutex_unlock(struct mutex_t * lock_)
 {
+    lock(&multithreading_lock);
     if (list_size(&lock_->locked_threads) == 0) {
         lock_->locked = 0;
     } else {
-        lock(&multithreading_lock);
         struct thread_t* thread = list_top(&lock_->locked_threads)->value;
         list_pop_front(&lock_->locked_threads, nothing);
         list_push_back(&running_threads, thread);
-        unlock(&multithreading_lock);
     }
+    unlock(&multithreading_lock);
 }
