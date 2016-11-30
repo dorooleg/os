8eafd392212feb75c03d8941cdc630dfdf847c24
Oleg Doronin <dorooleg@yandex.ru>
2016-11-30 16:59:35 +0300

    Out the int

--- inc/ints.h
+++ inc/ints.h
@@ -2,18 +2,14 @@
 #define __INTS_H__
 #include <printf.h>
 
-int cnt;
 static inline void disable_ints(void)
 { 
     __asm__ volatile ("cli" : : : "cc"); 
-    cnt++;
 }
 
 static inline void enable_ints(void)
 { 
-    cnt--;
-    if (cnt == 0)
-        __asm__ volatile ("sti" : : : "cc"); 
+    __asm__ volatile ("sti" : : : "cc"); 
 }
 
 #endif /*__INTS_H__*/
--- src/spinlock.c
+++ src/spinlock.c
@@ -2,9 +2,11 @@
 #include <printf.h>
 #include <ints.h>
 
+static int icnt;
 void lock(struct spinlock * lock)
 {
     disable_ints();
+    icnt++;
     do
     {
         while (atomic_load_explicit(&lock->locked, memory_order_acquire) == LOCKED);
@@ -14,5 +16,7 @@ void lock(struct spinlock * lock)
 void unlock(struct spinlock * lock)
 {
     atomic_store_explicit(&lock->locked, UNLOCKED, memory_order_release);
-    enable_ints();
+    icnt--;
+    if (icnt == 0)
+        enable_ints();
 }
